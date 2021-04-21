fd817013eea0449793c6cb1e8b3bf1532ec3802e
O. O. Nathan <noguntuberu@atbtechsoft.com>
2021-04-21 16:04:37 +0100

    updated routes/_config

--- src/routes/_config.js
+++ src/routes/_config.js
@@ -4,26 +4,26 @@
 
 let router = require('express').Router();
 let {
-    handle_404,
-    handle_error,
-    setup_request,
-    process_response,
+    handle404,
+    handleError,
+    setupRequest,
+    processResponse,
 } = require('../middlewares/http');
 
 /** Route Handlers */
 let sample_route_handler = require('./sample');
 
 /** Cross Origin Handling */
-router.use(setup_request);
+router.use(setupRequest);
 router.use('/samples', sample_route_handler);
-router.use(process_response);
+router.use(processResponse);
 
 /** Static Routes */
 router.use('/image/:image_name', (request, response) => {
 
 });
 
-router.use(handle_404);
-router.use(handle_error);
+router.use(handle404);
+router.use(handleError);
 
 module.exports = router;
